fb8eb03157b35fcb6bcc83e1237d343477408134
Oneharry <harryofficialchidi@yahoo.com>
2023-05-27 11:35:56 +0100

    added client environmental var files

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,22 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import {Home, Discover, Profile, Event, Login, Register} from './pages/index';
+import { AuthProvider } from "./context/context";
 
 function App() {
   return (
     <div className="App">
+      <AuthProvider>
         <BrowserRouter>
-          <Routes>
-            <Route exact path="/" element={<Home />} />
-            <Route exact path="/event" element={<Event />} />
-            <Route exact path="/discover" element={<Discover />} />
-            <Route exact path="/profile" element={<Profile />} />
-            <Route exact path="/login" element={<Login />} />
-            <Route exact path="/register" element={<Register />} />
-          </Routes>
-        </BrowserRouter>
+            <Routes>
+              <Route exact path="/" element={<Home />} />
+              <Route exact path="/event" element={<Event />} />
+              <Route exact path="/discover" element={<Discover />} />
+              <Route exact path="/profile" element={<Profile />} />
+              <Route exact path="/login" element={<Login />} />
+              <Route exact path="/register" element={<Register />} />
+            </Routes>
+          </BrowserRouter>
+      </AuthProvider>
     </div>
   );
 }
--- client/src/context/context.js
+++ client/src/context/context.js
@@ -39,7 +39,7 @@ export const AuthProvider = ({ children }) => {
       }
   
     return (
-      <AuthContext.Provider value={ value }>
+      <AuthContext.Provider value={ value } >
         {!loading && children}
       </AuthContext.Provider>
     );
