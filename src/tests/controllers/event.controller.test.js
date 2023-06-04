e7590c8fb8aebd65d5867b11dd08af49458f8490
Oneharry <harryofficialchidi@yahoo.com>
2023-06-04 14:43:54 +0100

    writing test

--- src/tests/controllers/event.controller.test.js
+++ src/tests/controllers/event.controller.test.js
@@ -1,78 +1,25 @@
-const { createEventByUser } = require('../../controllers');
-const {createEvent} = require('../../database')
-const { v4: uuidv4 } = require('uuid');
-const cloudinary = require('cloudinary');
+const { getAllEvents } = require('../../controllers');
+const {getEvents} = require('../../database')
 
-jest.mock('cloudinary');
+// Mocking the dependencies
+jest.mock('../../database', () => ({
+  getEvents: jest.fn().mockResolvedValue(['event1', 'event2']), // Mock the getEvents function
+}));
 
-describe('createEventByUser', () => {
-  const req = {
-    file: {
-      path: '/path/to/image.jpg',
-    },
-    body: {
-      name: 'Event Name',
-      category: 'Event Category',
-      venue: 'Event Venue',
-      description: 'Event Description',
-      price: 10.99,
-      totalTickets: 100,
-      start: '2023-06-01',
-      end: '2023-06-02',
-      organizer: 'Event Organizer',
-    },
-    params: {
-      userId: 'user123',
-    },
-  };
-
-  const res = {
-    status: jest.fn(() => res),
-    send: jest.fn(),
-  };
-
-  const form = {
-    eventId: expect.any(String),
-    name: req.body.name,
-    category: req.body.category,
-    venue: req.body.venue,
-    description: req.body.description,
-    amount: req.body.price,
-    totalTickets: req.body.totalTickets,
-    start: req.body.start,
-    end: req.body.end,
-    imageUrl: expect.any(String),
-    organizer: req.body.organizer,
-    userId: req.params.userId,
-  };
-
-  test('should create event and send response with success message', async () => {
-    const expectedResponse = {
-      message: 'Event created successfully',
-      data: expect.anything(),
+describe('getAllEvents', () => {
+  it('should return all events', async () => {
+    const req = {};
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
     };
 
-    const result = { secure_url: 'https://example.com/image.jpg' };
-    cloudinary.uploader.upload.mockResolvedValue(result);
+    await getAllEvents(req, res);
 
-    await createEventByUser(req, res);
-
-    expect(cloudinary.uploader.upload).toHaveBeenCalledWith(req.file.path);
-    expect(createEvent).toHaveBeenCalledWith(form);
-    expect(res.status).toHaveBeenCalledWith(201);
-    expect(res.send).toHaveBeenCalledWith(expectedResponse);
-  });
-
-  test('should handle error and send response with error message', async () => {
-    const error = new Error('Error creating event');
-    createEvent.mockRejectedValue(error);
-
-    await createEventByUser(req, res);
-
-    expect(res.status).toHaveBeenCalledWith(500);
-    expect(res.send).toHaveBeenCalledWith({
-      message: 'Error creating event',
-      error: error,
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.json).toHaveBeenCalledWith({
+      status: 'success',
+      data: ['event1', 'event2'],
     });
   });
-});
+})
