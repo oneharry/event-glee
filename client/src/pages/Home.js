af1507cf048d8058ab32422f0b395331225ec4e7
Oneharry <harryofficialchidi@yahoo.com>
2023-06-10 01:48:20 +0100

    added about and feature section in homepage

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -22,29 +22,25 @@ console.log(allEvents)
 
   
     return (
-      <div className='home'>
+      <div id='home'>
         {errmsg !== '' && <Display /> }
         <div className="event">
-          <div class="card bg-dark text-white" >
-            <img src="https://img.freepik.com/premium-photo/green-3d-abstract-motion-dark-background_53876-162900.jpg?w=740" class="card-img" alt="..." height="600" />
-            <div class="card-img-overlay d-flex flex-column justify-content-center w-75">
-            <h2 className=''>Discover and book events, concerts and more with EventGlee, the ultimate solution for securing tickets to the most exciting events in Nigeria</h2>
-            <p>Planning your event is just the beginning, we make your events discoverable and selling tickets easy to make your experience as smooth as possible</p>
-            </div>
-          </div>
-  
-          <div className="home-buttons">
+          <div class="card bg-dark text-dark border-0" >
+            <img src="https://images.pexels.com/photos/7794441/pexels-photo-7794441.jpeg?auto=compress&cs=tinysrgb&w=600" class="card-img" alt="..." height="600" />
+            <div class="card-img-overlay d-flex flex-column justify-content-center">
+              <div>
+              <h2 className='d-flex flex-column justify-content-center w-75'>Discover and book events, concerts and more with EventGlee, the ultimate solution for securing tickets to the most exciting events in Nigeria</h2>
+              <p>Planning your event is just the beginning, we make your events discoverable and selling tickets easy to make your experience as smooth as possible</p>
+              
+              </div>
+              <div className="home-buttons">
             <button className="home-discover-but">
               <Link  to="/discover" className="home-link">
                 Discover your next event
               </Link>
             </button>
-  
-            <button className="home-discover-but1">
-              <Link to="/event" className="home-link1">
-                Create your event
-              </Link>
-            </button>
+          </div>
+            </div>
           </div>
           <div className="category-section">
             <div className="home-category">
@@ -64,35 +60,41 @@ console.log(allEvents)
           </div>
         </div>
 
-        <section id='feature' className="home-section1">
-
-          <div class="card border-0 w-md-75 mb-3 px-md-5 py-md-3 d-flex justify-content-center" >
-            <div class="row g-0">
+        <section id='feature' className="home-section1 mx-md-5 px-md-5">
+        <div class="card-header my-1 bg-white border-0 fs-1 fw-bold">
+              Features
+            </div>
+          <div class="card border-0 w-md-75 my-md-2 py-md-3 d-flex justify-content-center align-items-center" >
+            <div class="row g-0 d-flex justify-content-center align-items-center">
               <div class="col-md-4">
                 <img src="https://images.pexels.com/photos/7551752/pexels-photo-7551752.jpeg" class="img-fluid rounded-start" alt="..." />
               </div>
               <div class="col-md-8 w-md-50">
-                <div class="card-body">
-                  <h5 class="card-title">Create Event</h5>
-                  <p class="card-text">Seamlessly plan and organize various types of events, including parties, conferences, meetings, workshops, concerts, picnics, and more. The intuitive interface and user-friendly tools simplify the entire event planning process</p>
-                  <Link to={"/event"} class="card-text"><small class="text-muted">Get started here</small></Link>
+                <div class="card-body text-center">
+                  <h4 class="card-title fs-2">Create Event</h4>
+                  <p class="card-text fs-5">Seamlessly plan and organize various types of events, including parties, conferences, meetings, workshops, concerts, picnics, and more. The intuitive interface and
+                    user-friendly tools simplify the entire event planning process. 
+                  </p>
+                  <Link to={"/event"} class="card-text fs-5"><small class="text-muted">Get started here</small></Link>
                 </div>
               </div>
             </div>
           </div>
 
-          <div class="card border-0 w-md-75 mb-3 px-md-5 py-md-3 d-flex justify-content-center" >
-            <div class="row g-0">
+          <div class="card border-0 w-md-75 my-md-4 py-md-3 d-flex justify-content-center align-items-center" >
+            <div class="row g-0 d-flex justify-content-center align-items-center">
               
-              <div class="col-md-8 w-md-50">
-                <div class="card-body">
-                  <h5 class="card-title">Tickets</h5>
-                  <p class="card-text">An awesome experience for attendees, allowing them to easily get tickets for their favourite event and receive digital printable tickets in their emails</p>
-                  <Link class="card-text"><small to={"/discover"} class="text-muted">Discover your events</small></Link>
+              <div class="col-md-8">
+                <div class="card-body text-center">
+                  <h4 class="card-title fs-2 ">Tickets</h4>
+                  <p class=" card-text fs-5 ">An awesome experience for attendees, allowing them to easily get tickets for their favourite event and receive digital printable tickets in their emails.
+                    Search for your favourite events by name or by categories.
+                  </p>
+                  <Link class="card-text fs-5"><small to={"/discover"} class="text-muted">Discover your events</small></Link>
                 </div>
               </div>
               <div class="col-md-4">
-                <img src="https://images.pexels.com/photos/68760/pexels-photo-68760.jpeg" class="img-fluid rounded-start" alt="..." />
+                <img src="https://images.pexels.com/photos/8261829/pexels-photo-8261829.jpeg?auto=compress&cs=tinysrgb&w=600" class="img-fluid rounded-start" alt="..." />
               </div>
             </div>
           </div>
@@ -100,30 +102,40 @@ console.log(allEvents)
 
         </section>
         <hr></hr>
-        <section id='about' className="home-section1">
-          <div class="card text-center">
-            <div class="card-header bg-white">
+        <section id='about' className="home-section1 mx-md-5 px-5">
+          <div class="card text-center border-0 my-2">
+            <div class="card-header bg-white fs-1 fw-bold border-0">
               About
             </div>
-            <div class="card-body">
-              <h5 class="card-title">Special title treatment</h5>
-              <p class="card-text">
-                <p>
-                  This web app was inspired by a personal need to seamlessly organize a social event, which would allow interested persons to register for the event. Also, the learning and deep curiosity of building a full-stack application using Nodejs
-                </p>
-                This project is part of my curriculum, to portfolio project that showcases my soft and technical skills, knowledge and creativity in developing software solutions.
-                This project is so special to me because of the amount of hardwork, dedication, learning and growth throughout my ALX journey.
-              </p>
-              <p>
-                The timeline for this project spanned across 3 weeks of intensive research and 3 weeks of actual development
-              </p>
-              <p>
-                As time went on, the goal shifted from just building a functional full-stack application
-                to creating an application that makes life easier for users. 
-              </p>
-              <a href="#" class="btn btn-primary">Go somewhere</a>
+            <div class="card-group border-0 my-2">
+              <div class="card d-flex justify-content-center align-items-center border-0">
+                <div class="card-body">
+                  <h4 class="card-title fs-2">Why this project?</h4>
+                  <p className='fs-5'>
+                    This project is part of my curriculum, to portfolio project that showcases my soft and technical skills, knowledge and creativity in developing software solutions.
+                    This project is so special to me because of the amount of hardwork, dedication, learning and growth throughout my ALX journey.
+                  </p>
+                  </div>
+              </div>
+              <div class="card border-0 d-flex justify-content-center align-items-center">
+                
+                <div class="card-body ">
+                  <h4 class="card-title fs-2">Timeline</h4>
+                  <p className='fs-5'>
+                    The timeline for this project spanned across 3 weeks of intensive research and 3 weeks of actual development
+                  </p>
+                 </div>
+              </div>
+              <div class="card border-0 d-flex justify-content-center align-items-center">
+                <div class="card-body">
+                  <h4 class="card-title fs-2">Contribution</h4>
+                    <p className='fs-5'>
+                      You can reach out to me on my socials <span><a href='https://www.linkedin.com/in/ezugwu-harrison-b37493127/'>LinkedIn</a> | <a href='https://twitter.com/chd_harrisn'>Twitter</a> | <a href='https://github.com/oneharry'>Github</a></span>
+                    </p>
+                    <p>The source code and project documentation can be found in the github repository for the project. Source code <a href='https://github.com/oneharry/event-glee'>here</a></p>
+                  </div>
+              </div>
             </div>
-            
           </div>
         </section>
         {/* { <Footer /> } */}
